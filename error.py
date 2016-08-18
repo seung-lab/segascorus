9777c1c53e83f50e401ea6131a45d2e202a853aa
Ignacio Tartavull <tartavull@princeton.edu>
2016-08-18 10:30:29 -0400

    Deciding which importing method to used base on file extension

--- error.py
+++ error.py
@@ -450,6 +450,23 @@ def import_h5(filename):
     f = h5py.File(filename)
     return f['/main'].value.astype(DTYPE)
 
+def import_file(filename):
+	'''
+	Takes a path to a file in the form of an string and decides which import
+	method to use base on the filename extension.
+	If it doesn't find a suitable import method raises ValueError
+	'''
+	valid_h5_extensions = ['.h5','.hdf5']
+	valid_tif_extensions = ['.tif','.tiff']
+	_, file_extension = path.splitext(filename)
+	if file_extension in valid_h5_extensions:
+		return import_h5(filename)
+	elif file_extension in valid_tif_extensions:
+		return import_tif(filename)
+	else:
+		raise ValueError('Failed to load filename {} ,Valid file extensions are {}'
+											.format(filename, valid_h5_extensions+valid_tif_extensions))
+
 
 #=====================================================================
 #Main function (script functionality)
@@ -471,8 +488,8 @@ def main(seg1_fname, seg2_fname,
 	'''
 
 	print "Loading Data..."
-	seg1 = import_tif(seg1_fname)
-	seg2 = import_tif(seg2_fname)
+	seg1 = import_file(seg1_fname)
+	seg2 = import_file(seg2_fname)
 
 	results = {}
 
